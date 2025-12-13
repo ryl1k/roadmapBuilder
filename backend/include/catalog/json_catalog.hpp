2d6d723e910f07fb2e4f124dea10fe519b2f50b8
ryl1k <ruslan4ukshev4yk@gmail.com>
2025-12-13 20:52:36 +0200

    remove sqlite and json implementations, switch to postgres only

--- backend/include/catalog/json_catalog.hpp
+++
@@ -1,8 +0,0 @@
-#include "icatalog.hpp"
-
-class JsonCatalog : public ICatalog {
-	std::string path;
-public:
-	explicit JsonCatalog(const std::string& path) : path(path) {}
-	std::vector<Course> getAll() override;
-};
